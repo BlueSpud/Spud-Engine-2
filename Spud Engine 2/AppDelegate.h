c8cce2272a23b7b2bdf5fa9e9c56522bb47137cd
Logan <logan2299@yahoo.com>
2016-08-31 18:35:43 -0400

    Initial Commit

---
+++ Spud Engine 2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Spud Engine 2
+//
+//  Created by Logan Pazol on 8/31/16.
+//  Copyright © 2016 Logan Pazol. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Spud Engine 2/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Spud Engine 2
+//
+//  Created by Logan Pazol on 8/31/16.
+//  Copyright © 2016 Logan Pazol. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
